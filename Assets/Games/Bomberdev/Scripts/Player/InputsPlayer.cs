8dc3a57b49df6dd33af94dea4e2a93b311418305
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-28 20:16:20 -0300

    separação de responsabilidades

---
+++ Assets/Games/Bomberdev/Scripts/Player/InputsPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputsPlayer : MonoBehaviour {
+    private Move move;
+    
+    private void Start() {
+        move = GetComponent<Move>();
+    }
+
+    private void Update() {
+        InputMove();
+    }
+
+    private void InputMove() {
+        if (Input.GetKeyDown(Keys.up)) move.Translate(Direction.UP);
+        if (Input.GetKeyDown(Keys.down)) move.Translate(Direction.DOWN);
+        if (Input.GetKeyDown(Keys.left)) move.Translate(Direction.LEFT);
+        if (Input.GetKeyDown(Keys.right)) move.Translate(Direction.RIGHT);
+    }
+}
--- Assets/Games/Bomberdev/Scripts/Player/Move.cs
+++ Assets/Games/Bomberdev/Scripts/Player/Move.cs
@@ -11,26 +11,33 @@ public class Move : MonoBehaviour {
     }
 
     private void Update() {
-        InputMove();
         Run();
     }
 
-    private void InputMove() {
-        if (Input.GetKeyDown(Keys.up)) EnqueuePosition(new Vector2(0, 1));
-        if (Input.GetKeyDown(Keys.down)) EnqueuePosition(new Vector2(0, -1));
-        if (Input.GetKeyDown(Keys.left)) EnqueuePosition(new Vector2(-1, 0));
-        if (Input.GetKeyDown(Keys.right)) EnqueuePosition(new Vector2(1, 0));
-    }
+    public void Translate(Direction direction) {
+        Vector2 translate = Vector2.zero;
+        switch(direction) {
+            case Direction.UP:
+                translate = new Vector2(0, 1);
+                break;
+            case Direction.DOWN:
+                translate = new Vector2(0, -1);
+                break;
+            case Direction.LEFT:
+                translate = new Vector2(-1, 0);
+                break;
+            case Direction.RIGHT:
+                translate = new Vector2(1, 0);
+                break;
+        }
 
-    private void EnqueuePosition(Vector2 move) {
         Vector2 position;
         if (queuePositions.Count > 0) {
-            position = queuePositions[queuePositions.Count - 1] + move;
+            position = queuePositions[queuePositions.Count - 1] + translate;
         } else {
-            position = ((Vector2) transform.position) + move;
+            position = ((Vector2) transform.position) + translate;
         }
         queuePositions.Add(position);
-        print(position);
     }
 
     private void Run() {
