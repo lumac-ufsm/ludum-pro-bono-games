2c614b028a657e15b72cb98f511fce3a61ae7ba8
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-29 16:32:35 -0300

    movimentação do bomberdev

--- Assets/Games/Bomberdev/Scripts/Player/Move.cs
+++ Assets/Games/Bomberdev/Scripts/Player/Move.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour {
-    private List<Vector2> queuePositions = new List<Vector2>();
+    private List<Translation> translations = new List<Translation>();
     private Vector2 oldPosition;
+    private Rigidbody2D rigidbody2D;
 
     private void Start() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
         oldPosition = transform.position;
+
+        Translate(Direction.LEFT);
+        Translate(Direction.LEFT);
+        Translate(Direction.LEFT);
+        Translate(Direction.UP);
     }
 
     private void Update() {
         Run();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Wall")) {
+            Stop();
+        }
     }
 
     public void Translate(Direction direction) {
@@ -32,34 +45,50 @@ public class Move : MonoBehaviour {
         }
 
         Vector2 position;
-        if (queuePositions.Count > 0) {
-            position = queuePositions[queuePositions.Count - 1] + translate;
+        if (translations.Count > 0) {
+            position = translations[translations.Count - 1].position + translate;
         } else {
             position = ((Vector2) transform.position) + translate;
         }
         position = new Vector2(Mathf.Round(position.x), Mathf.Round(position.y));
-        queuePositions.Add(position);
+        translations.Add(new Translation(direction, position));
     }
 
     private void Run() {
-        if (queuePositions.Count > 0) {
+        if (translations.Count > 0) {
             Vector2 currentPosition = transform.position;
-            Vector2 nextPosition = queuePositions[0];
+            Vector2 nextPosition = translations[0].position;
+            Direction direction = translations[0].direction;
 
             bool endTranslate = currentPosition == nextPosition;
 
-            if (nextPosition.x > oldPosition.x) endTranslate = currentPosition.x >= nextPosition.x;
-            else if (nextPosition.x < oldPosition.x) endTranslate = currentPosition.x <= nextPosition.x;
-            else if (nextPosition.y > oldPosition.y) endTranslate = currentPosition.y >= nextPosition.y;
-            else if (nextPosition.y < oldPosition.y) endTranslate = currentPosition.y <= nextPosition.y;
+            if (direction == Direction.RIGHT) endTranslate = currentPosition.x >= nextPosition.x;
+            else if (direction == Direction.LEFT) endTranslate = currentPosition.x <= nextPosition.x;
+            else if (direction == Direction.UP) endTranslate = currentPosition.y >= nextPosition.y;
+            else if (direction == Direction.DOWN) endTranslate = currentPosition.y <= nextPosition.y;
 
             if (endTranslate) {
                 oldPosition = nextPosition;
-                transform.position = nextPosition;
-                queuePositions.RemoveAt(0);
+                Stop();
             } else {
-                transform.Translate((nextPosition - oldPosition) * 0.1f);
+                if (direction == Direction.DOWN || direction == Direction.UP) {
+                    rigidbody2D.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+                } else if (direction == Direction.LEFT || direction == Direction.RIGHT) {
+                    rigidbody2D.constraints &= ~RigidbodyConstraints2D.FreezePositionX;
+                }
+                var force = nextPosition - currentPosition;
+                rigidbody2D.AddForce(force, ForceMode2D.Impulse);
             }
         }
+    }
+
+    private void Stop() {
+        rigidbody2D.Sleep();
+        rigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+        Vector2 position = transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+        transform.position = position;
+        if (translations.Count > 0) translations.RemoveAt(0);
     }
 }
