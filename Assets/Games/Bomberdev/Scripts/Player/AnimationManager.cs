a54ad4d7715ad0c97415a9ddb2c8307db2d1b6af
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-29 17:39:52 -0300

    anim

---
+++ Assets/Games/Bomberdev/Scripts/Player/AnimationManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour {
+    private Animator animator;
+    private Rigidbody2D rigidbody2D;
+    
+    private void Start() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if (rigidbody2D.velocity == Vector2.zero) {
+            animator.SetBool("stopped", true);
+        } else {
+            animator.SetBool("stopped", false);
+            animator.SetFloat("velocityX", rigidbody2D.velocity.x);
+            animator.SetFloat("velocityY", rigidbody2D.velocity.y);
+        }
+    }
+}
