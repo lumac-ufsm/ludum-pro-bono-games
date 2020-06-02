d2588bd40ceb682c4acec1a81c39f817167d3924
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-02 19:16:20 -0300

    Update Flowchart.cs

--- Assets/Games/Bomberdev/Scripts/Flowchart/Flowchart.cs
+++ Assets/Games/Bomberdev/Scripts/Flowchart/Flowchart.cs
@@ -3,5 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flowchart : MonoBehaviour {
-    
+    private List<GameObject> instructions = new List<GameObject>();
+
+    private void Start() {
+        foreach (Transform child in transform) {
+            GameObject instruction = child.gameObject;
+            if (instruction.GetComponent<Instruction>()) {
+                instructions.Add(instruction);
+            }
+        }
+
+        print(instructions.Count);
+    }
 }
