7620207966734de9ac93c9fcfbb52032b14c0a0f
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-03 23:34:38 -0300

    Update GameManagerBomberdev.cs

--- Assets/Games/Bomberdev/Scripts/GameManager/GameManagerBomberdev.cs
+++ Assets/Games/Bomberdev/Scripts/GameManager/GameManagerBomberdev.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManagerBomberdev : MonoBehaviour {
     [SerializeField] private PanelFlowchart panelFlowchart;
@@ -9,11 +10,21 @@ public class GameManagerBomberdev : MonoBehaviour {
     private void Update() {
         Inputs();
     }
+
     private void Inputs() {
         if (Input.GetKeyDown(Keys.up)) panelFlowchart.MoveOrSelect(Direction.UP);
         if (Input.GetKeyDown(Keys.down)) panelFlowchart.MoveOrSelect(Direction.DOWN);
         if (Input.GetKeyDown(Keys.left)) panelFlowchart.MoveOrSelect(Direction.LEFT);
         if (Input.GetKeyDown(Keys.right)) panelFlowchart.MoveOrSelect(Direction.RIGHT);
         if (Input.GetKeyDown(Keys.action1)) panelFlowchart.ToggleHold();
+        if (Input.GetKeyDown(Keys.start)) Run();
+    }
+
+    private void Run() {
+        foreach(GameObject instructionGameObject in flowchartRun.instructions) {
+            Image image = instructionGameObject.transform.GetComponentInChildren<Image>();
+            Text text = image.GetComponentInChildren<Text>();
+            print(text.text);
+        }
     }
 }
