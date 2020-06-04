43acbea19364f95838ba2f2a325b9e926edea5e4
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-03 23:15:38 -0300

    prefabs variant + gamemanager

---
+++ Assets/Games/Bomberdev/Scripts/GameManagerBomberdev.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerBomberdev : MonoBehaviour {
+    [SerializeField] private Flowchart flowchart;
+}
