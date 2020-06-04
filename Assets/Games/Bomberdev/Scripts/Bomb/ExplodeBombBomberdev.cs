1384ad24c9928bc9f0607477b4b27e28b3ef15d9
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-04 02:08:47 -0300

    Update ExplodeBombBomberdev.cs

--- Assets/Games/Bomberdev/Scripts/Bomb/ExplodeBombBomberdev.cs
+++ Assets/Games/Bomberdev/Scripts/Bomb/ExplodeBombBomberdev.cs
@@ -3,5 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplodeBombBomberdev : MonoBehaviour {
-    
+    public float delay = 3;
+    public int power = 1;
+    private float time = 0;
+
+    private void Update() {
+        time += Time.deltaTime;
+        if (time >= delay) Explode(); 
+    }
+
+    private void Explode() {
+        Destroy(gameObject);
+    }
 }
