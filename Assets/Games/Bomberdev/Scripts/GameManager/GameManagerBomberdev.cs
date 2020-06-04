06a647f9b4911bfe0be83e0d05214e03bf76d03e
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-04 01:24:42 -0300

    script separado para controle dos comandos

--- Assets/Games/Bomberdev/Scripts/GameManager/GameManagerBomberdev.cs
+++ Assets/Games/Bomberdev/Scripts/GameManager/GameManagerBomberdev.cs
@@ -5,18 +5,10 @@ using UnityEngine.UI;
 
 public class GameManagerBomberdev : MonoBehaviour {
     [SerializeField] private PanelFlowchartBomberdev panelFlowchart;
-    [SerializeField] private FlowchartBomberdev flowchartRun;
-    [SerializeField] private GameObject player;
-    private MovePlayerBomberdev movePlayer;
-    private Func.Callback callbackEndCommand;
-    private Queue<CommandBomberdev> commands;
-
-    private void Start() {
-        movePlayer = player.GetComponent<MovePlayerBomberdev>();
-        callbackEndCommand = () => ExecuteNextCommand();
-    }
+    private RunCommandsBomberdev runCommandsBomberdev;
 
     private void Update() {
+        runCommandsBomberdev = GetComponent<RunCommandsBomberdev>();
         Inputs();
     }
 
@@ -26,40 +18,6 @@ public class GameManagerBomberdev : MonoBehaviour {
         if (Input.GetKeyDown(Keys.left)) panelFlowchart.MoveOrSelect(Direction.LEFT);
         if (Input.GetKeyDown(Keys.right)) panelFlowchart.MoveOrSelect(Direction.RIGHT);
         if (Input.GetKeyDown(Keys.action1)) panelFlowchart.ToggleHold();
-        if (Input.GetKeyDown(Keys.start)) StartCommands();
-    }
-
-    private void StartCommands() {
-        UpdateCommands();
-        ExecuteNextCommand();
-    }
-
-    private void UpdateCommands() {
-        commands = new Queue<CommandBomberdev>();
-        foreach(GameObject instructionGameObject in flowchartRun.instructions) {
-            InstructionBomberdev instruction = instructionGameObject.GetComponent<InstructionBomberdev>();
-            commands.Enqueue(instruction.command);
-        }
-    }
-
-    private void ExecuteNextCommand() {
-        if (commands.Count > 0) ExecuteCommand(commands.Dequeue());
-    }
-
-    private void ExecuteCommand(CommandBomberdev command) {
-        switch(command) {
-            case CommandBomberdev.UP:
-                movePlayer.Translate(Direction.UP, callbackEndCommand);
-                break;
-            case CommandBomberdev.DOWN:
-                movePlayer.Translate(Direction.DOWN, callbackEndCommand);
-                break;
-            case CommandBomberdev.LEFT:
-                movePlayer.Translate(Direction.LEFT, callbackEndCommand);
-                break;
-            case CommandBomberdev.RIGHT:
-                movePlayer.Translate(Direction.RIGHT, callbackEndCommand);
-                break;
-        }
+        if (Input.GetKeyDown(Keys.start)) runCommandsBomberdev.StartCommands();
     }
 }
---
+++ Assets/Games/Bomberdev/Scripts/GameManager/RunCommandsBomberdev.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunCommandsBomberdev : MonoBehaviour {
+    [SerializeField] private FlowchartBomberdev flowchartRun;
+    private Queue<CommandBomberdev> commands;
+    private Func.Callback callbackEndCommand;
+    private MovePlayerBomberdev movePlayer;
+    [SerializeField] private GameObject player;
+
+    private void Start() {
+        movePlayer = player.GetComponent<MovePlayerBomberdev>();
+        callbackEndCommand = () => ExecuteNextCommand();
+    }
+
+    public void StartCommands() {
+        UpdateCommands();
+        ExecuteNextCommand();
+    }
+
+    private void UpdateCommands() {
+        commands = new Queue<CommandBomberdev>();
+        foreach(GameObject instructionGameObject in flowchartRun.instructions) {
+            InstructionBomberdev instruction = instructionGameObject.GetComponent<InstructionBomberdev>();
+            commands.Enqueue(instruction.command);
+        }
+    }
+
+    private void ExecuteNextCommand() {
+        if (commands.Count > 0) ExecuteCommand(commands.Dequeue());
+    }
+
+    private void ExecuteCommand(CommandBomberdev command) {
+        switch(command) {
+            case CommandBomberdev.UP:
+                movePlayer.Translate(Direction.UP, callbackEndCommand);
+                break;
+            case CommandBomberdev.DOWN:
+                movePlayer.Translate(Direction.DOWN, callbackEndCommand);
+                break;
+            case CommandBomberdev.LEFT:
+                movePlayer.Translate(Direction.LEFT, callbackEndCommand);
+                break;
+            case CommandBomberdev.RIGHT:
+                movePlayer.Translate(Direction.RIGHT, callbackEndCommand);
+                break;
+        }
+    }
+
+}
