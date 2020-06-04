012fe6bb5de57ae89d9b184c5bbc091038c32914
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-04 02:15:59 -0300

    Explosion

---
+++ Assets/Games/Bomberdev/Scripts/Bomb/ExplosionBomberdev.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionBomberdev : MonoBehaviour {
+    public float delay = 1;
+    private float time = 0;
+
+    private void Update() {
+        time += Time.deltaTime;
+        if (time >= delay) AutoDestroy(); 
+    }
+
+    private void AutoDestroy() {
+        Destroy(gameObject);
+    }
+}
