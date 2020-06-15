fed8bef0f4350266ef1805bf2c6bd495cd26ff21
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-15 02:09:22 -0300

    refactor

--- Assets/Games/Bomberdev/Scripts/Enemies/BomberdevDeathEnemy.cs
+++ Assets/Games/Bomberdev/Scripts/Enemies/BomberdevDeathEnemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Death : MonoBehaviour {
+public class BomberdevDeathEnemy : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Explosion")) {
             Destroy(gameObject);
