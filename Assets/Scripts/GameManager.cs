497a3967ff6996eccd5e009d487ad06bca3cccac
luanws <luan.w.silveira@gmail.com>
2020-09-27 23:40:13 -0300

    GameManager não é destruído

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Global : MonoBehaviour {
+public class GameManager : MonoBehaviour {
+    private static GameManager instance = null;
     void Start() {
         Cursor.visible = false;
+    }
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
     }
 
     void Update() {
