e5be6d69ff95e7d969957b4dbf0fba498bb97440
luanws <luan.w.silveira@gmail.com>
2020-09-27 23:15:59 -0300

    Game over

---
+++ Assets/Games/SnakeMath/Scripts/GameOverSnakeMath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverSnakeMath : MonoBehaviour {
+    public Text textPontuacao;
+
+    void Start() {
+        textPontuacao.text = "Pontuação: " + SnakeStorage.points;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(Keys.start)) {
+            SceneManager.LoadScene("Games/SnakeMath/Scenes/Main");
+        }
+    }
+}
