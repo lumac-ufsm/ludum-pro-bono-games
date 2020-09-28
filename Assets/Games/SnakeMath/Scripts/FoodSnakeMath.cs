79238642926668d33563155756f84d6be2c37884
luanws <luan.w.silveira@gmail.com>
2020-09-28 03:57:54 -0300

    foods manager

--- Assets/Games/SnakeMath/Scripts/FoodSnakeMath.cs
+++ Assets/Games/SnakeMath/Scripts/FoodSnakeMath.cs
@@ -1,17 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 
 public class FoodSnakeMath : MonoBehaviour {
     [SerializeField] private int _value;
     public int value { get { return _value; } }
-    private SnakeSnakeMath snake;
-    private GameAreaSnakeMath gameArea;
-
+    private FoodsManagerSnakeMath foodsManager;
+    
     private void Start() {
-        snake = GameObject.Find("Snake").GetComponent<SnakeSnakeMath>();
-        gameArea = GameObject.Find("GameArea").GetComponent<GameAreaSnakeMath>();
+        foodsManager = GameObject.Find("GameManagerSnakeMath").GetComponent<FoodsManagerSnakeMath>();
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -21,19 +18,7 @@ public class FoodSnakeMath : MonoBehaviour {
     }
 
     private void Reposition() {
-        var snakePositions = snake.bodyList.Select(body => (Vector2) body.transform.position);
-        Vector2[] invalidPositions = snakePositions.ToArray();
-        transform.position = SortPositionFood(invalidPositions);
-    }
-
-    private Vector2 SortPositionFood(Vector2[] invalidPositions) {
-        Vector2 position;
-        do {
-            position = new Vector2(
-                Mathf.Round(Random.Range(gameArea.minX, gameArea.maxX)),
-                Mathf.Round(Random.Range(gameArea.minY, gameArea.maxY))
-            );
-        } while (position == null || invalidPositions.Contains(position));
-        return position;
+        Vector2[] invalidPositions = foodsManager.GetInvalidPositions();
+        transform.position = foodsManager.SortPositionFood(invalidPositions);
     }
 }
--- Assets/Games/SnakeMath/Scripts/GameAreaSnakeMath.cs
+++ Assets/Games/SnakeMath/Scripts/GameAreaSnakeMath.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameAreaSnakeMath : MonoBehaviour {
-    private float screenHeight;
-    private float screenWidth;
     public int marginTop;
-    private float _maxX, _minX, _maxY, _minY;
 
-    public float maxX { get { return _maxX; } }
-    public float minX { get { return _minX; } }
-    public float maxY { get { return _maxY; } }
-    public float minY { get { return _minY; } }
+    public float maxX { get { return (screenWidth - 1) / 2; } }
+    public float minX { get { return -maxX; } }
+    public float maxY { get { return ((screenHeight - 1) / 2) - marginTop; } }
+    public float minY { get { return -maxY - (marginTop * 2); } }
+    public float screenHeight { 
+        get {
+            var (screenHeight, screenWidth) = GetScreen();
+            return screenHeight;
+        }
+    }
+    public float screenWidth { 
+        get {
+            var (screenHeight, screenWidth) = GetScreen();
+            return screenWidth;
+        }
+    }
 
-    private void Start() {
-        screenHeight = Camera.main.orthographicSize * 2;
-        screenWidth = screenHeight / Screen.height * Screen.width;
+    private (float, float) GetScreen() {
+        float screenHeight = Camera.main.orthographicSize * 2;
+        float screenWidth = screenHeight / Screen.height * Screen.width;
 
         screenHeight = Mathf.Floor(screenHeight);
         screenHeight += screenHeight % 2 == 0 ? -1 : 0;
@@ -23,15 +32,11 @@ public class GameAreaSnakeMath : MonoBehaviour {
         screenWidth += screenWidth % 2 == 0 ? -1 : 0;
         screenHeight -= marginTop * 2;
 
+        return (screenHeight, screenWidth);
+    }
+
+    private void Start() {
         gameObject.transform.Translate(new Vector2(0, -marginTop));
-
-        _maxX = (screenWidth - 1) / 2;
-        _minX = -_maxX;
-        _maxY = ((screenHeight - 1) / 2);
-        _minY = -_maxY;
-        _maxY -= marginTop;
-        _minY -= marginTop;
-
         gameObject.transform.localScale = new Vector2(screenWidth, screenHeight);
     }
 }
