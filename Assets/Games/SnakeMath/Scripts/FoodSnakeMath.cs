84a23f7980ae363999b57da8dc7ab61c32fca19c
luanws <luan.w.silveira@gmail.com>
2020-09-28 02:43:51 -0300

    foods

---
+++ Assets/Games/SnakeMath/Scripts/FoodSnakeMath.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class FoodSnakeMath : MonoBehaviour {
+    [SerializeField] private int _value;
+    public int value { get { return _value; } }
+    private SnakeSnakeMath snake;
+    private GameAreaSnakeMath gameArea;
+
+    private void Start() {
+        snake = GameObject.Find("Snake").GetComponent<SnakeSnakeMath>();
+        gameArea = GameObject.Find("GameArea").GetComponent<GameAreaSnakeMath>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            Reposition();
+        }
+    }
+
+    private void Reposition() {
+        var snakePositions = snake.bodyList.Select(body => (Vector2) body.transform.position);
+        Vector2[] invalidPositions = snakePositions.ToArray();
+        transform.position = SortPositionFood(invalidPositions);
+    }
+
+    private Vector2 SortPositionFood(Vector2[] invalidPositions) {
+        Vector2 position;
+        do {
+            position = new Vector2(
+                Mathf.Round(Random.Range(gameArea.minX, gameArea.maxX)),
+                Mathf.Round(Random.Range(gameArea.minY, gameArea.maxY))
+            );
+        } while (position == null || invalidPositions.Contains(position));
+        return position;
+    }
+}
--- Assets/Games/SnakeMath/Scripts/SnakeSnakeMath.cs
+++ Assets/Games/SnakeMath/Scripts/SnakeSnakeMath.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class SnakeSnakeMath : MonoBehaviour {
     [SerializeField] GameObject bodyPrefab;
-    private List<GameObject> bodyList;
-    public int points { get { return bodyList.Count; } }
+    private List<GameObject> _bodyList;
+    public List<GameObject> bodyList { get { return _bodyList; } }
+    public int points { get { return _bodyList.Count; } }
     public bool isStart {
         get {
             Vector2 position = transform.position;
-            foreach (GameObject body in bodyList) {
+            foreach (GameObject body in _bodyList) {
                 Vector2 bodyPosition = body.transform.position;
                 if (bodyPosition != position) return false;
                 position = bodyPosition;
@@ -19,16 +20,16 @@ public class SnakeSnakeMath : MonoBehaviour {
     }
     
     private void Start() {
-        bodyList = new List<GameObject>();
+        _bodyList = new List<GameObject>();
         AddBody();
     }
 
     public void Move(Vector2 translation) {
-        if (bodyList.Count > 0) {
-            for (int n = bodyList.Count - 1; n > 0; n--) {
-                bodyList[n].transform.position = bodyList[n - 1].transform.position;
+        if (_bodyList.Count > 0) {
+            for (int n = _bodyList.Count - 1; n > 0; n--) {
+                _bodyList[n].transform.position = _bodyList[n - 1].transform.position;
             }
-            bodyList[0].transform.position = transform.position;
+            _bodyList[0].transform.position = transform.position;
         }
         transform.Translate(translation);
     }
@@ -36,7 +37,7 @@ public class SnakeSnakeMath : MonoBehaviour {
     public void AddBody(int num=1) {
         for (int n = 0; n < num; n++) {
             GameObject body = Instantiate(bodyPrefab, transform.position, Quaternion.identity);
-            bodyList.Add(body);
+            _bodyList.Add(body);
         }
     }
 }
