bfe903c426584b7722007b9d4f168735b81421ea
luanws <luan.w.silveira@gmail.com>
2020-12-07 01:25:52 -0300

    fix(pacmaze): bug portal

--- Assets/Games/Pacmaze/Scripts/Player/DeathPlayerPacmaze.cs
+++ Assets/Games/Pacmaze/Scripts/Player/DeathPlayerPacmaze.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DeathPlayerPacmaze : MonoBehaviour {
+    private Vector2 initialPosition;
+
+    private void Start() {
+        initialPosition = transform.position;
+    }
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Enemy")) {
-            gameObject.transform.position = new Vector2(-20.5f,-12.5f);
+            gameObject.transform.position = initialPosition;
         }
     }
 }
