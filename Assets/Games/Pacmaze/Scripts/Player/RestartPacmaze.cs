09f8910626b01310a2520cce4e078e8b74dfeade
Robert Rafael <57976387+RR-Art@users.noreply.github.com>
2020-12-21 11:22:39 -0300

    Pacmaze Restart

---
+++ Assets/Games/Pacmaze/Scripts/Player/RestartPacmaze.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartPacmaze : MonoBehaviour {
+    private Vector2 initialPosition;
+
+    private void Start() {
+        initialPosition = transform.position;
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.D)){
+            gameObject.transform.position = initialPosition;
+        }
+    }
+}
