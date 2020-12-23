b9928180569dcd5cd6d6b18ce667cce044f61166
luanws <luan.w.silveira@gmail.com>
2020-12-23 13:52:02 -0300

    feat(pacmaze): rotação do portal

---
+++ Assets/Games/Pacmaze/Scripts/Portal/RotatePortalPazmate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePortalPazmate : MonoBehaviour {
+    [SerializeField] private float speedRotation = 1f;
+    private void Update() {
+        transform.Rotate(new Vector3(0, 0, speedRotation * Time.deltaTime));
+    }
+}
