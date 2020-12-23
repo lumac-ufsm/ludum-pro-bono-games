2ded8106338797be581ae93907a5249f1dee4247
luanws <luan.w.silveira@gmail.com>
2020-12-23 14:01:48 -0300

    Update RestartPacmaze.cs

--- Assets/Games/Pacmaze/Scripts/Player/RestartPacmaze.cs
+++ Assets/Games/Pacmaze/Scripts/Player/RestartPacmaze.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class RestartPacmaze : MonoBehaviour {
     private Vector2 initialPosition;
+    private Rigidbody2D rigidbodyPacmaze;
 
     private void Start() {
         initialPosition = transform.position;
+        rigidbodyPacmaze = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update() {
-        if(Input.GetKeyDown(KeyCode.D)){
+        if(Input.GetKeyDown(Keys.action4)){
             gameObject.transform.position = initialPosition;
+            rigidbodyPacmaze.velocity = Vector2.zero;
         }
     }
 }
