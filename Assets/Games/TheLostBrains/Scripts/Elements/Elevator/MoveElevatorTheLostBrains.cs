0f760480877f8127c6f5547bd4274bbe3490f9fb
Luan Willig Silveira <luan.w.silveira@gmail.com>
2021-05-12 15:22:44 -0300

    feat(the-lost-brains/elevator): movimento do elevador

--- Assets/Games/TheLostBrains/Scripts/Elements/Elevator/MoveElevatorTheLostBrains.cs
+++ Assets/Games/TheLostBrains/Scripts/Elements/Elevator/MoveElevatorTheLostBrains.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveElevatorTheLostBrains : MonoBehaviour {
+	[SerializeField] private float modSpeed = 0;
+	[SerializeField] private float breakTime = 0;
+	[SerializeField] private ElevatorStateTheLostBrains state;
+	private float timeCount = 0;
+
 	void Start() {
 
 	}
 
 	void Update() {
+		if (timeCount > 0) {
+			timeCount -= Time.deltaTime;
+		} else {
+			float speed = GetSpeed();
+			transform.Translate(new Vector2(0, speed * Time.deltaTime));
+		}
+	}
 
+	private float GetSpeed() {
+		switch (state) {
+			case ElevatorStateTheLostBrains.UP:
+				return modSpeed;
+			case ElevatorStateTheLostBrains.DOWN:
+				return -modSpeed;
+			default:
+				return 0;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		ElevatorStateTheLostBrains oldState = state;
+		if (other.gameObject.name == "TopPoint") {
+			state = ElevatorStateTheLostBrains.DOWN;
+			if (oldState != state) timeCount = breakTime;
+		} else if (other.gameObject.name == "BottomPoint") {
+			state = ElevatorStateTheLostBrains.UP;
+			if (oldState != state) timeCount = breakTime;
+		}
 	}
 }
