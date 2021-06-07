46a14e2b3682182d31a036876d9a0bc64c6ee6b9
luanws <luan.w.silveira@gmail.com>
2021-06-07 15:31:34 -0300

    Update RunCommandsBomberdev.cs

--- Assets/Games/Bomberdev/Scripts/GameManager/RunCommandsBomberdev.cs
+++ Assets/Games/Bomberdev/Scripts/GameManager/RunCommandsBomberdev.cs
@@ -3,60 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RunCommandsBomberdev : MonoBehaviour {
-    private Queue<(CommandBomberdev, GameObject)> commands;
-    private CommandManagerBomberdev commandManager;
-    private Func.Callback callbackEndCommand;
-    private MovePlayerBomberdev movePlayer;
-    [SerializeField] private GameObject bombPrefab;
-    [SerializeField] private FlowchartBomberdev flowchartRun;
-    private GameObject player;
+	private Queue<(CommandBomberdev, GameObject)> commands;
+	private CommandManagerBomberdev commandManager;
+	private Func.Callback callbackEndCommand;
+	private MovePlayerBomberdev movePlayer;
+	[SerializeField] private GameObject bombPrefab;
+	[SerializeField] private FlowchartBomberdev flowchartRun;
+	private GameObject player;
 
-    private void Start() {
-        player = GameObject.Find("Player");
-        movePlayer = player.GetComponent<MovePlayerBomberdev>();
-        callbackEndCommand = () => ExecuteNextCommand();
-        commandManager = GetComponent<CommandManagerBomberdev>();
-        commandManager.callback = callbackEndCommand;
-    }
+	private void Start() {
+		player = GameObject.Find("Player");
+		movePlayer = player.GetComponent<MovePlayerBomberdev>();
+		callbackEndCommand = () => ExecuteNextCommand();
+		commandManager = GetComponent<CommandManagerBomberdev>();
+		commandManager.callback = callbackEndCommand;
+	}
 
-    public void StartCommands() {
-        UpdateCommands();
-        ExecuteNextCommand();
-    }
+	public void StartCommands() {
+		UpdateCommands();
+		ExecuteNextCommand();
+	}
 
-    private void UpdateCommands() {
-        commands = new Queue<(CommandBomberdev, GameObject)>();
-        foreach(GameObject instructionGameObject in flowchartRun.instructions) {
-            InstructionBomberdev instruction = instructionGameObject.GetComponent<InstructionBomberdev>();
-            commands.Enqueue((instruction.command, instructionGameObject));
-        }
-    }
+	private void UpdateCommands() {
+		commands = new Queue<(CommandBomberdev, GameObject)>();
+		foreach (GameObject instructionGameObject in flowchartRun.instructions) {
+			InstructionBomberdev instruction = instructionGameObject.GetComponent<InstructionBomberdev>();
+			commands.Enqueue((instruction.command, instructionGameObject));
+		}
+	}
 
-    private void ExecuteNextCommand() {
-        var (command, gameObject) = commands.Dequeue();
-        if (commands.Count > 0) ExecuteCommand(command, gameObject);
-    }
+	private void ExecuteNextCommand() {
+		if (commands.Count > 0) {
+			var (command, gameObject) = commands.Dequeue();
+			ExecuteCommand(command, gameObject);
+		}
+	}
 
-    private void ExecuteCommand(CommandBomberdev command, GameObject instructionGameObject) {
-        switch(command) {
-            case CommandBomberdev.UP:
-                commandManager.Up(movePlayer);
-                break;
-            case CommandBomberdev.DOWN:
-                commandManager.Down(movePlayer);
-                break;
-            case CommandBomberdev.LEFT:
-                commandManager.Left(movePlayer);
-                break;
-            case CommandBomberdev.RIGHT:
-                commandManager.Right(movePlayer);
-                break;
-            case CommandBomberdev.BOMB:
-                commandManager.Bomb(player.transform.position, bombPrefab);
-                break;
-            case CommandBomberdev.WAIT:
-                commandManager.Wait(3);
-                break;
-        }
-    }
+	private void ExecuteCommand(CommandBomberdev command, GameObject instructionGameObject) {
+		switch (command) {
+			case CommandBomberdev.UP:
+				commandManager.Up(movePlayer);
+				break;
+			case CommandBomberdev.DOWN:
+				commandManager.Down(movePlayer);
+				break;
+			case CommandBomberdev.LEFT:
+				commandManager.Left(movePlayer);
+				break;
+			case CommandBomberdev.RIGHT:
+				commandManager.Right(movePlayer);
+				break;
+			case CommandBomberdev.BOMB:
+				commandManager.Bomb(player.transform.position, bombPrefab);
+				break;
+			case CommandBomberdev.WAIT:
+				commandManager.Wait(3);
+				break;
+		}
+	}
 }
