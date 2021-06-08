540a1c4f2b9f642340cf8bfc1cb6fb2fdaffe725
Robert Rafael <57976387+RR-Art@users.noreply.github.com>
2021-06-08 16:41:40 -0300

    Insert TextOfSteps

--- Assets/Games/Bomberdev/Scripts/Flowchart/Instructions/MoveInstructionBomberdev.cs
+++ Assets/Games/Bomberdev/Scripts/Flowchart/Instructions/MoveInstructionBomberdev.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class MoveInstructionBomberdev : MonoBehaviour {
 	[SerializeField] private int _numberOfSteps = 1;
+	[SerializeField] private Text txtOfSteps;
 
 	public int numberOfSteps {
 		get { return _numberOfSteps; }
 	}
 
 	void Start() {
-
+		if (_numberOfSteps == 1){
+			txtOfSteps.text = "";
+		} else {
+			txtOfSteps.text = ((char)_numberOfSteps);
+		}
 	}
 }
