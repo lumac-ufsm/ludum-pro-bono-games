7f0869f9a1d810a7653b1656d7d4054d23de990d
luanws <luan.w.silveira@gmail.com>
2021-07-21 22:24:32 -0300

    feat(ranking): exibição do ranking dos jogos

---
+++ Assets/Main/Scripts/Ranking.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Ranking : MonoBehaviour {
+	[SerializeField] private GameSelectorManager gameSelectorManager;
+	[SerializeField] private TextMeshPro rankingTextMeshPro;
+	private GameName gameName;
+
+	private void Start() {
+		gameName = gameSelectorManager.gameName;
+		UpdateRanking();
+	}
+
+	private void UpdateRanking() {
+		StartCoroutine(ScoreClient.GetTopScores((int)gameName, scores => {
+			string rankingText = "";
+			foreach (Score score in scores) {
+				string name = score.user.name;
+				int scoreNumber = score.score;
+				string institutionName = score.user.institution.name;
+				rankingText += $"{institutionName} {name} {scoreNumber}\n";
+			}
+			rankingTextMeshPro.text = rankingText;
+		}));
+	}
+}
